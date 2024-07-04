95b27c3e0b377bd814dbbbdb63e9173a43a03bc3
nonamenoname <lex>
2024-07-04 16:39:35 +0300

    #4

--- src/js/Symbol.js
+++ src/js/Symbol.js
@@ -22,10 +22,12 @@ export default class Symbol {
 
   static get symbols() {
 return [
+  "bincoin_isometric_logo_3d",
   "bitcoin-btc-cryptocurrency-svgrepo-com",
   "ethereum-crypto-cryptocurrency-svgrepo-com",
   "monero-crypto-cryptocurrency-coins-svgrepo-com",
-  "ripple-xrp-cryptocurrency-svgrepo-com"
+  "ripple-xrp-cryptocurrency-svgrepo-com",
+  "tether-crypto-cryptocurrency-svgrepo-com",
 ];
 }
 
--- symbolsList.js
+++
@@ -1,9 +0,0 @@
-export const symbols = [
-  "bincoin_isometric_logo_3d",
-  "bitcoin-btc-cryptocurrency-svgrepo-com",
-  "ethereum-crypto-cryptocurrency-svgrepo-com",
-  "monero-crypto-cryptocurrency-coins-svgrepo-com",
-  "ripple-xrp-cryptocurrency-svgrepo-com",
-  "tether-crypto-cryptocurrency-svgrepo-com",
-  "tie_ln"
-];
